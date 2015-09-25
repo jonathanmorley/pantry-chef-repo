7371d4f9f255dd498b4ee80e10086f57c441ab50
Morley, Jonathan <jmorley@cvent.com>
2015-09-25 11:39:00 -0400

    Add packages

--- Policyfile.rb
+++ Policyfile.rb
@@ -1,40 +1,16 @@
-# Policyfile.rb - Describe how you want Chef to configure your workstation.
-#
-# For more information on the Policyfile feature, visit
-# https://github.com/opscode/chef-dk/blob/master/POLICYFILE_README.md
-
 name 'pantry'
-# Get cookbooks from supermarket.chef.io
 default_source :community
-
-##########
-# Run List
-# chef-client will run these recipes in the order specified.
-# Modify this to include other cookbooks you wish to use, separating
-# each recipe name with commas. For example:
-#
-# run_list(
-#   'pantry',
-#   'mycookbook'
-# )
-#
-# Add `cookbook` entries for cookbooks that are not found on
-# supermarket. See the POLICYFILE_README.md for more information.
 
 run_list(
   'pantry'
 )
 
-############
-# Attributes
-# Feel free to modify these values, or add your own attributes for
-# other cookbooks.
-# Specify values as a space separated list of words. For example,
-# %w(git go packer tree)
-#
-# packages for OS X
-default['homebrew']['casks']      = %w(git skype cord)
-default['homebrew']['formula']    = %w()
-default['homebrew']['taps']       = %w()
-# packages for Windows
-default['chocolatey']['packages'] = %w()
+default['homebrew'].tap do |brew|
+  brew['taps'] = %w(caskroom/cask caskroom/fonts)
+  brew['formulas'] =  %w(fish git jenv nvm rbenv)
+  brew['casks'] = %w(atom dockertoolbox dropbox flux font-input google-chrome slack spotify vagrant vagrant-manager)
+end
+
+default['chocolatey'].tap do |choco|
+  choco['packages'] = %w(atom git powershell)
+end
